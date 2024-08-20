47a05de3fd863170234b470e56b76ec18d3868ac
Its Me Hara <17094719+itsmehara@users.noreply.github.com>
2024-08-20 10:42:32 +0530

    Create main.tf

---
+++ Proj4/google/main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "example" {
+  name         = "google-example-instance"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
