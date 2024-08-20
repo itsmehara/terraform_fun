372960105ddd098952574032180ae1395f9ffee7
Its Me Hara <17094719+itsmehara@users.noreply.github.com>
2024-08-20 10:23:51 +0530

    Create main.tf

---
+++ Proj2/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "example-instance"
+  }
+}
