329d2cd67b8ebc4e75e8468460e6381546d341fe
Its Me Hara <17094719+itsmehara@users.noreply.github.com>
2024-08-20 10:47:21 +0530

    Create variables.tf

---
+++ Proj4/multi-providers/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  default     = "us-west-2"
+}
+
+variable "google_project" {
+  description = "The Google Cloud project ID"
+  default     = "my-gcp-project-id"
+}
